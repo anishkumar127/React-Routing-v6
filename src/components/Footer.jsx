6a0ea8be992818983eabfaa1ddd880d207d09027
Anish Kumar <anishbishnoi127@gmail.com>
2023-03-03 18:47:20 +0530

    navbar fixed

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-dark text-light p-4 d-flex  align-items-center justify-content-center">
+      Footer Session
+    </div>
+  );
+};
+
+export default Footer;
